59f7df27de5188d754d4c8e50784693d388dfeb4
SebastianRoether <Sebastian.Roether@googlemail.com>
2018-12-06 02:35:55 +0100

    Fixed "KeyNotFoundException" in MqttAdapter
    Subscribe and unsubscribe will wait for the subAck and unsubAck packets but did not consider the possibility that these packets could be received before writing the requests was done, hence the completions sources were already deleted from the dictionary when trying to access them later

--- iothub/device/src/Transport/Mqtt/MqttIotHubAdapter.cs
+++ iothub/device/src/Transport/Mqtt/MqttIotHubAdapter.cs
@@ -450,11 +450,12 @@ namespace Microsoft.Azure.Devices.Client.Transport.Mqtt
             {
                 int packetId = Util.GetNextPacketId();
                 var subscribePacket = new SubscribePacket(packetId, new SubscriptionRequest(topicFilter, qos));
-                this.subscribeCompletions[packetId] = new TaskCompletionSource();
+                var subscribeCompletion = new TaskCompletionSource();
+                this.subscribeCompletions[packetId] = subscribeCompletion;
 
                 await Util.WriteMessageAsync(context, subscribePacket, ShutdownOnWriteErrorHandler).ConfigureAwait(true);
 
-                await this.subscribeCompletions[packetId].Task.ConfigureAwait(true);
+                await subscribeCompletion.Task.ConfigureAwait(true);
             }
 
             if (Logging.IsEnabled) Logging.Exit(this, context.Name, packetPassed, nameof(SubscribeAsync));
@@ -488,11 +489,12 @@ namespace Microsoft.Azure.Devices.Client.Transport.Mqtt
             int packetId = Util.GetNextPacketId();
             packetPassed.PacketId = packetId;
 
-            this.unsubscribeCompletions[packetId] = new TaskCompletionSource();
+            var unsubscribeCompletion = new TaskCompletionSource();
+            this.unsubscribeCompletions[packetId] = unsubscribeCompletion;
 
             await Util.WriteMessageAsync(context, packetPassed, ShutdownOnWriteErrorHandler).ConfigureAwait(true);
 
-            await this.unsubscribeCompletions[packetId].Task.ConfigureAwait(true);
+            await unsubscribeCompletion.Task.ConfigureAwait(true);
 
             if (Logging.IsEnabled) Logging.Exit(this, context.Name, packetPassed, nameof(UnSubscribeAsync));
         }
